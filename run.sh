1b2f8ad0d05d6960ded689dc5b567382bfbb8321
WorkSteve7284 <93044233+WorkSteve7284@users.noreply.github.com>
2026-06-06 13:48:24 -0400

    Improved setup process

--- run.sh
+++ run.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
-cd $(dirname $0)
+cleanup() {
+    local exit_code=$?
 
+    echo "Running cleanup"
+
+    rm "$(dirname $0)/camera_lock.lck"
+
+    exit $exit_code
+}
+
+trap cleanup EXIT
+
+if [ -f "$(dirname $0)/camera_lock.lck" ]; then
+    echo "This program is already running somewhere else!"
+    exit 1
+fi
+
+touch "$(dirname $0)/camera_lock.lck"
+cd $(dirname $0)
 ./.venv/bin/python3 main.py
--- setup/setup.sh
+++ setup/setup.sh
@@ -1,32 +1,100 @@
 #!/bin/bash
 
-# This script is can only be run on the actual coprocessor itself.
-# This project requires Linux
+topname="$(dirname $0)/.."
 
-cd "$(dirname $0)/.."
+help() {
+    echo "$(basename "$(dirname $0)/..")" Setup Usage
+    echo "--help (-h, -?)     : Open this help menu."
+    echo ""
+    echo "One of the following must be selected."
+    echo "--runtime (default) : Create the environment required for running this software only."
+    echo "--testing           : Create the environment required for running unit tests and other testing software only."
+    echo "--develop           : Create the environment required for running, testing, and developing this software."
+    echo "--clear             : Erases the environment."
 
-# Install Picamera2; nessecary for Luma P1 support.
+}
+
+create_venv() {
+    venv_dir="$topname/.venv"
+    python3 -m venv --system-site-packages $venv_dir
+    echo "Created Python virtual environment at $venv_dir"
+}
+
+install_package() {
+    $topname/.venv/bin/python3 -m pip install $1
+    echo "Installed package $1"
+}
+
+install_robotpy() {
+    .venv/bin/python3 -m pip install --extra-index-url=https://wpilib.jfrog.io/artifactory/api/pypi/wpilib-python-release-2026/simple robotpy
+    .venv/bin/python3 -m pip install --extra-index-url=https://wpilib.jfrog.io/artifactory/api/pypi/wpilib-python-release-2026/simple robotpy-cscore
+    .venv/bin/python3 -m pip install --extra-index-url=https://wpilib.jfrog.io/artifactory/api/pypi/wpilib-python-release-2026/simple robotpy-apriltag
+}
+
+create_service() {
+    sed "s|@location@|$topname|" "$topname/setup/vision.service.template" | sudo tee /etc/systemd/system > /dev/null
+    sudo systemctl daemon-reload
+    sudo systemctl enable vision.service
+    sudo systemctl start vision.service
+}
+
+while getopts ":h?-:" opt; do
+    case "$opt" in
+        h|\?)
+            help
+            exit 0
+            ;;
+        -)
+            case "$OPTARG" in
+                runtime|-runtime)
+                    echo "Creating runtime environment!"
+                    sudo apt install -y python3-picamera2 --no-install-recommends
+                    create_venv
+                    install_package opencv-contrib-python-headless
+                    install_package matplotlib
+                    create_service
+                    exit 0
+                    ;;
+                testing|-testing)
+                    echo "Creating testing environment!"
+                    create_venv
+                    install_package pytest
+                    install_package opencv-contrib-python-headless
+                    exit 0
+                    ;;
+                develop|-develop)
+                    echo "Creating development environment!"
+                    create_venv
+                    install_package pytest
+                    install_package opencv-contrib-python-headless
+                    install_package matplotlib
+                    exit 0
+                    ;;
+                help|-help)
+                    help
+                    exit 0
+                    ;;
+                clear|-clear)
+                    echo "Erasing the environment!"
+                    rm -rf "$topname/.venv/"
+                    exit 0
+                    ;;
+                *)
+                    echo "Unknown option --$OPTARG! Run $(basename $0) --help for usage!"
+                    exit 1
+                    ;;
+            esac
+            ;;
+        *)
+            echo "Unknown option $opt! Run $(basename $0) --help for usage!"
+            exit 1
+            ;;
+    esac
+done
+
+echo "Creating runtime environment!"
 sudo apt install -y python3-picamera2 --no-install-recommends
-
-# Create python virtual environment
-python3 -m venv --system-site-packages .venv
-pydir="./.venv/bin/python3"
-
-# Install required PIP packages
-
-# Install robotpy
-# Note: This is likely to error between build seasons! If it does, resolve the issues manually.
-$pydir -m pip install --extra-index-url=https://wpilib.jfrog.io/artifactory/api/pypi/wpilib-python-release-2026/simple robotpy
-$pydir -m pip install --extra-index-url=https://wpilib.jfrog.io/artifactory/api/pypi/wpilib-python-release-2026/simple robotpy-cscore
-$pydir -m pip install --extra-index-url=https://wpilib.jfrog.io/artifactory/api/pypi/wpilib-python-release-2026/simple robotpy-apriltag
-
-# Install opencv & other packages
-$pydir -m pip install opencv-contrib-python-headless
-$pydir -m pip install matplotlib
-
-echo "Created Virtual Environment. It is located at $pydir"
-
-# Enable systemd service
-sudo cp setup/rpivision.service /etc/systemd/system/
-sudo systemctl daemon-reload
-echo "Note: if this code isn't run on a photonvision device, the systemd service (at /etc/systemd/system/rpivision.service) will need to be modified."
+create_venv
+install_package opencv-contrib-python-headless
+install_package matplotlib
+create_service
